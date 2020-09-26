b35d589236b89df20add8f24b756a6c2aa419ef7
VivekRavikumar <vivek2spell@gmail.com>
2020-09-26 16:14:41 +0530

    Spring initializers

---
+++ src/main/java/com/newsinfo/initializer/InitializerApplication.java
@@ -0,0 +1,13 @@
+package com.newsinfo.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerApplication.class, args);
+	}
+
+}
